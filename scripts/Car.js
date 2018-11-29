9c20ec6315b9c64b24cd8940ac1d9a3b919f6027
Luka Tašler <tasler.luka@gmail.com>
2018-11-29 20:21:25 +0100

    Update funkcija za Car

--- scripts/Car.js
+++ scripts/Car.js
@@ -6,9 +6,9 @@ function Car(position, rotation, speed, rotationSpeed) {
 	this.positon = typeof position !== 'undefined' ? position : [0.0, 0.0, 0.0];
 	this.speed = typeof speed !== 'undefined' ? speed : 0.0;
 	this.rotation = typeof rotation !== 'undefined' ? rotation : 0.0;
-	this.rotationSpeed = 0.2;
-	this.mvMatrix = mat4.create();
-	this.brakeSpeed = 10.0;
+	this.rotationSpeed = typeof rotationSpeed !== 'undefined' ? rotationSpeed : 0.2;
+	this.BRAKE_SPEED = 10.0;
+	this.DRAG_SPEED = 0.2;
 }
 
 Car.prototype.accelerate = function(deltaTime) {
@@ -17,11 +17,27 @@ Car.prototype.accelerate = function(deltaTime) {
 };
 
 Car.prototype.brake = function(deltaTime) {
-	this.speed = this.speed - this.brakeSpeed * deltaTime;
+	this.speed = this.speed - this.BRAKE_SPEED * deltaTime;
 };
 
 Car.prototype.rotation = function(direction, deltaTime) {
-	this.rotation = this.rotation + direction * this.rotationSpeed * deltaTime;
+	this.rotation[1] = this.rotation[1] + direction * this.rotationSpeed * deltaTime;
+};
+
+Car.prototype.update = function(deltaTime) {
+	var x = Math.cos(this.rotation[1]) * Math.cos(this.rotation[2]);
+	var y = Math.sin(this.rotation[1]) * Math.cos(this.rotation[2]);
+	var z = Math.sin(this.rotation[2]);
+
+	var direction = vec3.create()
+	direction = [x, y, z];
+
+	this.speed -= this.DRAG_SPEED * deltaTime;
+	var offset = vec3.create();
+	vec3.scale(offset, direction, this.speed);
+
+	vec3.add(this.positon, this.positon, offset);
+
 };
 
 Car.prototype.draw = function() {
