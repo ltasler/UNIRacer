920c271bb2fd46763dce33641d176efc8487bba2
Luka Tašler <tasler.luka@gmail.com>
2018-11-30 21:02:27 +0100

    bump

--- scripts/main.js
+++ scripts/main.js
@@ -80,7 +80,7 @@ function drawScene() {
 
 	mat4.identity(mvMatrix);
 	mat4.translate(mvMatrix, CAMERA_OFFSET);
-	mat4.rotateX(mvMatrix, degToRad(CAMERA_ROTATION_X);
+	mat4.rotateX(mvMatrix, degToRad(CAMERA_ROTATION_X));
 
 	// lowpoly Kart rendering
 	mvPushMatrix();
